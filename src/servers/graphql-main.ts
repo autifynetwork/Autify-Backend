ff4a18b26ccf7bd1672adff9de02cc4a0eacff0e
Supriyo Sarkar <supriyosarkar282@gmail.com>
2023-01-10 03:08:50 +0530

    feat: basic express server setup (#5)
    * feat: changed start script to graphql & set the node engine
    
    Signed-off-by: spantheslayer <supriyosarkar282@gmail.com>
    
    * del: deleted the scratch boilerplate
    
    Signed-off-by: spantheslayer <supriyosarkar282@gmail.com>
    
    * feat: added basic express server
    
    Signed-off-by: spantheslayer <supriyosarkar282@gmail.com>
    
    Signed-off-by: spantheslayer <supriyosarkar282@gmail.com>

---
+++ src/servers/graphql-main.ts
@@ -0,0 +1,14 @@
+import express from "express"
+import baseLogger from "@services/logger"
+
+const startServer = () => {
+  const app = express()
+  const port = process.env.APP_PORT || 4000
+
+  app.get("/healthz", (_, r) => r.sendStatus(200))
+  app.listen(port, () => baseLogger.info("🚀 Server listening on port " + port))
+}
+
+if (require.main === module) {
+  startServer()
+}

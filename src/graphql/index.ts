69dc32c3f593ba672f5234d6b55cc30bc0b2abac
spantheslayer <supriyosarkar282@gmail.com>
2023-02-01 13:08:50 +0530

    auth properties adde
    Signed-off-by: spantheslayer <supriyosarkar282@gmail.com>

--- src/graphql/index.ts
+++ src/graphql/index.ts
@@ -1,13 +1,15 @@
+// eslint-disable-next-line import/no-unassigned-import
 import 'reflect-metadata'
 
 import { buildSchema } from 'type-graphql'
 
 import { VendorResolver } from '@graphql/resolvers/Vendor'
 import { BrandResolver } from '@graphql/resolvers/Brand'
+import { AuthResolver } from '@graphql/resolvers/Auth'
 
 const getSchema = async () => {
     const schema = await buildSchema({
-        resolvers: [VendorResolver, BrandResolver],
+        resolvers: [VendorResolver, BrandResolver, AuthResolver],
     })
     return schema
 }

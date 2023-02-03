26037c23e239366b09b712f71f918a5edd2f1155
spantheslayer <supriyosarkar282@gmail.com>
2023-02-03 19:57:22 +0530

    added match pattern logic

--- src/services/otp.ts
+++ src/services/otp.ts
@@ -8,20 +8,29 @@ import { otp as otpConfig } from '@config'
 type CachedOTPSpec = {
     otp: string
     eat: number
+    retries: number
 }
 
 export const createOTP = async (identifier: string): Promise<string> => {
-    const otp_numeric = crypto.randomInt(0, 1000000)
-    const otp = otp_numeric.toString().padStart(6, '0')
+    const saved = await redis.get(identifier)
+
+    // In Cache
+    if (saved) return (JSON.parse(saved) as CachedOTPSpec).otp
+
+    const otp_numeric = crypto.randomInt(0, Math.pow(10, otpConfig.length))
+    const otp = otp_numeric.toString().padStart(otpConfig.length, '0')
+
+    console.log(otp)
+    console.log(otpConfig.length)
 
     const detonate = ms(otpConfig.expiration_time)
-
     const cached_otp: CachedOTPSpec = {
         otp,
         eat: Date.now() + detonate,
+        retries: 0,
     }
 
-    await redis.set(identifier, JSON.stringify(cached_otp))
+    await redis.set(identifier, JSON.stringify(cached_otp), 'PX', detonate)
 
     return otp
 }
@@ -31,12 +40,23 @@ export const matchOTP = async (identifier: string, otp: string): Promise<boolean
 
     if (!saved) return false
 
-    const { otp: saved_otp, eat }: CachedOTPSpec = JSON.parse(saved)
+    const cached: CachedOTPSpec = JSON.parse(saved)
 
-    if (eat < Date.now()) {
+    // if retries or expired
+    if (cached.retries >= otpConfig.retries || cached.eat < Date.now()) {
         await redis.del(identifier)
         return false
     }
 
-    return otp === saved_otp
+    // if passed
+    if (otp === cached.otp) {
+        await redis.del(identifier)
+        return true
+    }
+
+    // If Failed
+    cached.retries++
+    await redis.set(identifier, JSON.stringify(cached), 'PX', cached.eat)
+
+    return false
 }

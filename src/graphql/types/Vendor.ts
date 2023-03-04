5d6775dcf52e58454c32558d54d4f28de4d283a4
spantheslayer <supriyosarkar282@gmail.com>
2023-03-04 20:20:48 +0530

    feat: added whitelisting final call
    Signed-off-by: spantheslayer <supriyosarkar282@gmail.com>

--- src/graphql/types/Vendor.ts
+++ src/graphql/types/Vendor.ts
@@ -1,13 +1,50 @@
-import { Field, ObjectType } from 'type-graphql'
+import { Field, ID, InputType, ObjectType } from 'type-graphql'
+import { TagObject } from '@graphql/types/Tag'
+import { Brand } from '@prisma/client'
 
-@ObjectType()
-export class Vendor {
+@InputType()
+export class CreateVendorInput {
+    @Field(() => ID)
+    id: string
+    @Field({ nullable: true })
+    name?: string
+
     @Field()
     email: string
+
+    @Field()
+    whitelist: string
+
+    @Field()
+    brandId: string
+
+    @Field(() => [String])
+    tags: TagObject[]
+}
+
+@ObjectType()
+export class VendorObject {
+    @Field(() => ID)
+    id: string
 
     @Field()
     name: string
 
     @Field()
-    whitelist: boolean
+    email: string
+
+    @Field()
+    whitelist: string
+
+    @Field(() => [TagObject])
+    tags: TagObject[]
+
+    @Field(() => [String])
+    brand: Brand
+
+    @Field(() => Date)
+    createdAt: Date
+
+    @Field(() => Date)
+    updatedAt: Date
 }

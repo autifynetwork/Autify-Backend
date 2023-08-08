a545626eb195dd4d4569cc35950fd833b9937e17
spantheslayer <supriyosarkar282@gmail.com>
2023-08-08 13:30:18 +0530

    chore: added profile update

--- src/graphql/resolvers/Profile.ts
+++ src/graphql/resolvers/Profile.ts
@@ -1,12 +1,12 @@
-import { Arg, Mutation, Query, Resolver } from 'type-graphql'
+import { Arg, Mutation, Resolver } from 'type-graphql'
 
-import { ProfileObject } from '@graphql/types/Profile'
+import { ProfileInput, ProfileObject } from '@graphql/types/Profile'
 import { Profile } from '@prisma/client'
 import prisma from '@services/prisma'
 
 @Resolver()
 export class ProfileResolver {
-    @Mutation(() => ProfileObject)
+    @Mutation(() => ProfileInput)
     async populateProfile(
         @Arg('email') email: string,
         @Arg('firstName') firstName: string,
@@ -31,5 +31,28 @@ export class ProfileResolver {
             })
         }
         return profileEmail
+    }
+
+    @Mutation(() => ProfileObject)
+    async updateProfile(
+        @Arg('id') id: string,
+        @Arg('firstName', { nullable: true }) firstName: string,
+        @Arg('lastName', { nullable: true }) lastName: string,
+        @Arg('phone', { nullable: true }) phone: string,
+        @Arg('bName', { nullable: true }) bName: string,
+        @Arg('bEmail', { nullable: true }) bEmail: string
+    ): Promise<Profile> {
+        const profile = await prisma.profile.update({
+            where: { id },
+            data: {
+                firstName,
+                lastName,
+                phone,
+                bName,
+                bEmail,
+                updatedAt: new Date(),
+            },
+        })
+        return profile
     }
 }
